178a5e6ffbca60b3276abbb514d9994989a56f9e
JLamzon <jlamzon@yahoo.com>
2022-10-18 00:24:22 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿//Jessie Lamzon
+//10/17/22
+//GitHubChallenge - Redo mini Challenges
+//Ask Questions - add data validation and play again
+
+
+
+
+Console.Clear();
+
+string playAgain = "yes";
+
+
+while (playAgain != "no")
+{
+Console.WriteLine("What is your name?");
+string myName = Console.ReadLine();
+
+
+Console.WriteLine("What time did you wake up?");
+string awake = Console.ReadLine();
+
+Console.WriteLine("Your name is " + myName + " and you woke up at " + awake + ".");
+
+Console.WriteLine("Would you like to play again? yes/no");
+playAgain = Console.ReadLine();
+
+}
+
+Console.WriteLine("Thanks for playing!");

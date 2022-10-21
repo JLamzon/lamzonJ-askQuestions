c4cb7f8506801d19f9bd52eca96e5c50f8e22025
JLamzon <jlamzon@yahoo.com>
2022-10-21 10:49:09 -0700

    added peer review to program

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 //10/17/22
 //GitHubChallenge - Redo mini Challenges
 //Ask Questions - add data validation and play again
+// Peer Review: Elizar Garcia, code works well. 
+// It has the while loop and tryparse. Easy to use 
+// and follow. Asking for a specific time format was unique 
+// definetly liked it and it would loop until you gave it what 
+// it was asking for. Very well done.
 
 
 
